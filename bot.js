6c6d9ed8602f73545ff398f21b5cfadc0aa99387
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-25 18:09:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ if(message.content ==='/join'){
 if(message.member.voice.channel){
 const connection = await message.member.voice.channel.join();
 const broadcast = client.voice.createBroadcast();
-const dispatcher = broadcast.play('./Infected Mushroom - The Pretender.mp3');
+const dispatcher = broadcast.play('https://drive.google.com/file/d/1mAAHTQXJfSiNdCeHe9Y-ZO3Lt5vHWaNj/view');
 connection.play(broadcast);
 }else{
 message.reply('Necesitas entrar a un canal primero');
