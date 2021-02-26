87cd83cca3026d3f24bc28870beb68b1cbfbc252
Jesus Manuel LS <51806782+JMLS15@users.noreply.github.com>
2021-02-25 23:07:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
-const {prefix, token} = require('./config.json');
+const {prefix} = require('./config.json');
 const client = new Discord.Client();
 client.commands= new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
